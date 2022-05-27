67850cad34c28eeeb5cf48c4b6765265ab15d1aa
Shechem <yungrichhoes@gmail.com>
2022-05-27 01:56:07 -0400

    Html files for bookings

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,10 @@ func routes(app *config.AppConfig) http.Handler{
 
 	mux.Get("/",handlers.Repo.Home)
 	mux.Get("/about",handlers.Repo.About)
+	// for go to allow html to find static things
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	mux.Handle("/static/*",http.StripPrefix("/static",fileServer))
 
 	return mux
 }
